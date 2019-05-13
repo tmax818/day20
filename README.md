0992c9f2e52fe79df94d0be383fb133810b53055
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-13 11:54:44 -0700

    add footer

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
+## Day 20
+
+This is how I did it.
+
+
+
+
+
+```bash
+$ mkdir day20
+$ cd day20
+$ create-react-app .
+```
+Then all kinds of shit happens with create-react-app
+
+```json
+ "homepage": "<>",
+```
+
 ```bash
 $ npm run deploy
+```
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Day 20</h1>
       </header>
+      <Footer />
     </div>
   );
 }
